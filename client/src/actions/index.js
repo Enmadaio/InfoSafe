88a3d980af792a6034dd8c787f8f23579ededba1
Thom Richardson <thomarichardson@gmail.com>
2018-08-17 17:42:06 -0700

    Added thunk middleware, WE GOT A BALANCE NOW ON THE WEB!!! No connection to the ethereum network other than the API!

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,30 @@
 import {GET_TOKEN_BALANCE} from "../constants/action-types";
 
-export const getTokenBalance = tokenBalance => ({
-    type: GET_TOKEN_BALANCE,
-    payload: tokenBalance
-});
+const returnTokenBalance = (tokenBalance) => {
+    return {
+        type: GET_TOKEN_BALANCE,
+        payload: tokenBalance
+    }
+};
+
+const fetchTokenBalance = async () => {
+    let response = await fetch('http://127.0.0.1:3000/api/getTokenBalance', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            account: '0x8a45A9916C1bf2118E1047ad20D8c4Ba776D9722'
+        })
+    });
+    return await response.json();
+};
+
+export const getTokenBalance = () => {
+    return (dispatch) => {
+        fetchTokenBalance().then(tokenBalance => {
+            dispatch(returnTokenBalance(tokenBalance));
+        });
+    }
+};
